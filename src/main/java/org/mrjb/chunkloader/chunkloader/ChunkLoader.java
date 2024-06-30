34034357f0609a28c86db1c7d8410b0f36dd8fc0
HongMJ1315 <gone_co_1008@yahoo.com.tw>
2024-06-30 22:43:18 +0800

    First Update

---
+++ src/main/java/org/mrjb/chunkloader/chunkloader/ChunkLoader.java
@@ -0,0 +1,77 @@
+package org.mrjb.chunkloader.chunkloader;
+
+import org.bukkit.Chunk;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ChunkLoader extends JavaPlugin {
+
+    private Set<String> loadedChunks = new HashSet<>();
+    private File configFile;
+    private FileConfiguration config;
+
+    @Override
+    public void onEnable() {
+        getLogger().info("ChunkLoaderPlugin has been enabled!");
+        createConfigFile();
+        loadChunksFromConfig();
+        this.getCommand("chunk").setExecutor(new CommandEvent(this));
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("ChunkLoaderPlugin has been disabled.");
+        saveChunksToConfig();
+    }
+
+    public Set<String> getLoadedChunks() {
+        return loadedChunks;
+    }
+
+    public void addLoadedChunk(String chunkKey) {
+        loadedChunks.add(chunkKey);
+        saveChunksToConfig();
+    }
+
+    public void removeLoadedChunk(String chunkKey) {
+        loadedChunks.remove(chunkKey);
+        saveChunksToConfig();
+    }
+
+    private void createConfigFile() {
+        configFile = new File(getDataFolder(), "config.yml");
+        if (!configFile.exists()) {
+            configFile.getParentFile().mkdirs();
+            saveResource("config.yml", false);
+        }
+        config = YamlConfiguration.loadConfiguration(configFile);
+    }
+
+    private void loadChunksFromConfig() {
+        loadedChunks = new HashSet<>(config.getStringList("loadedChunks"));
+        for (String chunkKey : loadedChunks) {
+            String[] parts = chunkKey.split(",");
+            int x = Integer.parseInt(parts[0]);
+            int z = Integer.parseInt(parts[1]);
+            String worldName = parts[2];
+            Chunk chunk = getServer().getWorld(worldName).getChunkAt(x, z);
+            chunk.setForceLoaded(true);
+        }
+    }
+
+    private void saveChunksToConfig() {
+        config.set("loadedChunks", new ArrayList<>(loadedChunks));
+        try {
+            config.save(configFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/org/mrjb/chunkloader/chunkloader/CommandEvent.java
@@ -0,0 +1,130 @@
+package org.mrjb.chunkloader.chunkloader;
+
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CommandEvent implements CommandExecutor {
+
+    private ChunkLoader plugin;
+
+    public CommandEvent(ChunkLoader plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("chunk")) {
+            if (args.length == 0) {
+                sendClickableMessage((Player) sender);
+                return true;
+            }
+            if (args.length > 0) {
+                if (args[0].equalsIgnoreCase("help")) {
+                    sendHelpMessage(sender);
+                    return true;
+                } else if (args[0].equalsIgnoreCase("reset")) {
+                    resetChunks(sender);
+                    return true;
+                } else if (args[0].equalsIgnoreCase("load") || args[0].equalsIgnoreCase("unload")) {
+                    if (args.length == 3) {
+                        int x = Integer.parseInt(args[1]);
+                        int z = Integer.parseInt(args[2]);
+                        String worldName = (sender instanceof Player) ? ((Player) sender).getWorld().getName() : "world";
+                        handleChunkLoadUnload(sender, args[0], x, z, worldName);
+                    } else if (sender instanceof Player) {
+                        Player player = (Player) sender;
+                        Chunk chunk = player.getLocation().getChunk();
+                        handleChunkLoadUnload(sender, args[0], chunk.getX(), chunk.getZ(), player.getWorld().getName());
+                    } else {
+                        sender.sendMessage("Usage: /chunk " + args[0] + " [x] [z]");
+                    }
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void sendClickableMessage(Player player) {
+        TextComponent loadMessage = new TextComponent("[load]");
+        loadMessage.setColor(net.md_5.bungee.api.ChatColor.GREEN);
+        loadMessage.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/chunk load"));
+        loadMessage.setBold(true);
+
+        TextComponent unloadMessage = new TextComponent("[unload]");
+        unloadMessage.setColor(net.md_5.bungee.api.ChatColor.RED);
+        unloadMessage.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/chunk unload"));
+        unloadMessage.setBold(true);
+
+        TextComponent space = new TextComponent("                ");
+
+        TextComponent message = new TextComponent();
+        message.addExtra(loadMessage);
+        message.addExtra(space);
+        message.addExtra(unloadMessage);
+
+        player.spigot().sendMessage(message);
+    }
+
+    private void sendHelpMessage(CommandSender sender) {
+        if (sender instanceof Player) {
+            sender.sendMessage("Chunk Loader Plugin Commands:");
+            sender.sendMessage("/chunk - Show clickable options to load or unload the current chunk.");
+            sender.sendMessage("/chunk load - Load the chunk you are currently in.");
+            sender.sendMessage("/chunk load [x] [z] - Load the specified chunk.");
+            sender.sendMessage("/chunk unload - Unload the chunk you are currently in.");
+            sender.sendMessage("/chunk unload [x] [z] - Unload the specified chunk.");
+            sender.sendMessage("/chunk reset - Reset all chunks to normal loading mode.");
+            sender.sendMessage("/chunk help - Show this help message.");
+            sender.sendMessage("By Mr_JB");
+        } else {
+            sender.sendMessage("Chunk Loader Plugin Commands:");
+            sender.sendMessage("/chunk load [x] [z] - Load the specified chunk.");
+            sender.sendMessage("/chunk unload [x] [z] - Unload the specified chunk.");
+            sender.sendMessage("/chunk reset - Reset all chunks to normal loading mode.");
+            sender.sendMessage("/chunk help - Show this help message.");
+            sender.sendMessage("By Mr_JB");
+        }
+    }
+
+    private void resetChunks(CommandSender sender) {
+        for (String chunkKey : plugin.getLoadedChunks()) {
+            String[] parts = chunkKey.split(",");
+            int x = Integer.parseInt(parts[0]);
+            int z = Integer.parseInt(parts[1]);
+            String worldName = parts[2];
+            Chunk chunk = Bukkit.getWorld(worldName).getChunkAt(x, z);
+            chunk.setForceLoaded(false);
+        }
+        plugin.getLoadedChunks().clear();
+        plugin.saveConfig();
+        sender.sendMessage("All chunks have been reset to normal loading mode.");
+    }
+
+    private void handleChunkLoadUnload(CommandSender sender, String action, int x, int z, String worldName) {
+        World world = Bukkit.getWorld(worldName);
+        if (world == null) {
+            sender.sendMessage("World not found: " + worldName);
+            return;
+        }
+        Chunk chunk = world.getChunkAt(x, z);
+        String chunkKey = x + "," + z + "," + worldName;
+        if (action.equalsIgnoreCase("load")) {
+            chunk.setForceLoaded(true);
+            plugin.addLoadedChunk(chunkKey);
+            sender.sendMessage("Chunk at " + x + ", " + z + " in world " + worldName + " is now force loaded.");
+        } else if (action.equalsIgnoreCase("unload")) {
+            chunk.setForceLoaded(false);
+            plugin.removeLoadedChunk(chunkKey);
+            sender.sendMessage("Chunk at " + x + ", " + z + " in world " + worldName + " is now unloaded.");
+        }
+    }
+}
